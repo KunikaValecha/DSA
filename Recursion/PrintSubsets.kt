9932d2954e898ca8f32904641b978b4af03f394d
KunikaValecha <KunikaValecha211097@gmail.com>
2022-11-21 14:46:16 +0530

    adding recusrsion problem

---
+++ Recursion/PrintSubsets.kt
@@ -0,0 +1,24 @@
+package Recursion
+
+class PrintSubsets {
+    companion object{
+        fun getSubsets(input:String, output:String){
+            var i = input
+            if(i.isEmpty()){
+                print(" '$output'")
+                return
+            }
+            var output1 = output
+            var output2 = output
+            output2 = i.first().toString()
+            i = i.substring(1, i.length)
+            getSubsets(i, output1)
+            getSubsets(i, output2)
+            return
+        }
+    }
+}
+
+fun main() {
+    PrintSubsets.getSubsets("ab", "")
+}
